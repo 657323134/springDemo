20cc43784d19b899fbeb7e7fa1efebb2363c7719
liuyan <liuyan@vpfinance.cn>
2017-08-31 18:06:21 +0800

    test

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,24 +1,34 @@
 import com.alibaba.fastjson.JSONObject;
+import sun.util.calendar.BaseCalendar;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by liuyan on 2017/7/24.
  */
 public class Test {
     public static void main(String[] args) {
-        /*String s= "{ \"relation\":[{\"goodsId\":19,\"map\":[{\"sim\":DDDFDFFD,\"imei\":DDD}]}]}";
-        JSONObject simJson = JSONObject.parseObject(s);
-        System.out.println(simJson);*/
-        new Test().test("刘岩","student1","student2");
+        Map<String, String> map = new HashMap<>();
+        for (int i = 0; i < 500000; i++) {
+            map.put("key" + i, "value" + i);
+        }
+        Date start = new Date();
+        Set<String> keys = map.keySet();
+        for (String key:keys) {
+            String s = key + ":" + map.get(key);
+        }
+        Date end = new Date();
+        System.out.println(end.getTime()-start.getTime());
+
+        for (Map.Entry<String, String> entry:map.entrySet()) {
+            String sa = entry.getKey() + "" + entry.getValue();
+        }
+        System.out.println(new Date().getTime()-end.getTime());
+
     }
 
-    private void test(String name ,String... students) {
-        System.out.println(name);
-        System.out.println("spring");
 
-//        for (String student: students
-//             ) {
-//            System.out.println(student);
-//        }
-//        for (int i=0;i<)
-    }
 }
